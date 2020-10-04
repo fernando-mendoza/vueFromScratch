beef593a9ef0c377e8850c042e4acba377358eb4
Fernando <55415857+fernando-mendoza@users.noreply.github.com>
2020-10-04 06:54:29 +0000

    components

--- Tutorial1/app.js
+++ Tutorial1/app.js
@@ -46,3 +46,26 @@ var app2 = new Vue({
       }
     }
   })
+
+  var app6 = new Vue({
+    el: '#app-6',
+    data: {
+      message: 'Hola Vue!'
+    }
+  })
+
+  Vue.component('todo-item', {  
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+  })
+
+  var app7 = new Vue({
+    el: '#app-7',
+    data: {
+      groceryList: [
+        { id: 0, text: 'Vegetales' },
+        { id: 1, text: 'Queso' },
+        { id: 2, text: 'Cualquier otra cosa que se supone que los humanos coman' }
+      ]
+    }
+  })
