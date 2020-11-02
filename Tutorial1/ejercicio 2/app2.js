fcbeebdf4d84247b26b3e3f844566a3fdffc04cb
fernando-mendoza <55415857+fernando-mendoza@users.noreply.github.com>
2020-11-02 17:49:59 -0600

    directives, v-bind, v-for

--- Tutorial1/ejercicio 2/app2.js
+++ Tutorial1/ejercicio 2/app2.js
@@ -4,6 +4,8 @@ new Vue({
     return {
       name: "Bitcoin",
       img: "https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=002",
+      changePercent: 0,
+      prices: [1000, 2314, 4813, 1746, 8164, 2715],
     };
   },
 });
