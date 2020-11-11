fa67073e6552c8e2b7854f6a305d2cb24c152da7
fernando-mendoza <55415857+fernando-mendoza@users.noreply.github.com>
2020-11-10 18:54:58 -0600

    other way to use v-for

--- Tutorial1/ejercicio 2/app2.js
+++ Tutorial1/ejercicio 2/app2.js
@@ -6,6 +6,16 @@ new Vue({
       img: "https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=002",
       changePercent: 0,
       prices: [1000, 2314, 4813, 1746, 8164, 2715],
+
+      pricesWithDays: [
+        { day: "Lunes", value: 8400 },
+        { day: "Martes", value: 7900 },
+        { day: "Miercoles", value: 8200 },
+        { day: "Jueves", value: 9000 },
+        { day: "Viernes", value: 9400 },
+        { day: "Sabado", value: 10000 },
+        { day: "Domingo", value: 10200 },
+      ],
     };
   },
 });
