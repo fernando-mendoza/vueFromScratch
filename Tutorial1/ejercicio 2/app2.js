04eeec99d3019b017442028e98cac96d8e742b06
fernando-mendoza <55415857+fernando-mendoza@users.noreply.github.com>
2020-11-10 19:41:59 -0600

    new directive, v-on: to handle DOM events

--- Tutorial1/ejercicio 2/app2.js
+++ Tutorial1/ejercicio 2/app2.js
@@ -5,8 +5,6 @@ new Vue({
       name: "Bitcoin",
       img: "https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=002",
       changePercent: 0,
-      prices: [1000, 2314, 4813, 1746, 8164, 2715],
-
       pricesWithDays: [
         { day: "Lunes", value: 8400 },
         { day: "Martes", value: 7900 },
@@ -16,6 +14,13 @@ new Vue({
         { day: "Sabado", value: 10000 },
         { day: "Domingo", value: 10200 },
       ],
+
+      showPrices: false,
     };
+  },
+  methods: {
+    toggleShowPrices() {
+      this.showPrices = !this.showPrices;
+    },
   },
 });
